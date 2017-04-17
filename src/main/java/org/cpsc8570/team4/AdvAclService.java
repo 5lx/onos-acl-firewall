efaeec8bc545a7af4f4c3cca4fe7b1c54f53af2d
hcrdtc <huang2chao1ran2@gmail.com>
2017-04-17 06:05:14 -0400

    Add new method for aclService

--- src/main/java/org/cpsc8570/team4/AdvAclService.java
+++ src/main/java/org/cpsc8570/team4/AdvAclService.java
@@ -1,4 +1,15 @@
 package org.cpsc8570.team4;
 
+import java.util.List;
+
 public interface AdvAclService {
+
+    List<AdvAclRule> getAdvAclRules();
+
+    AdvAclRule addAdvAclRule(AdvAclRule rule);
+
+    void clearByRuleId(AdvAclRuleId ruleId);
+
+    void clearAll();
+
 }
